cae774bd12bf3740d6e6fee381d9dc4c8675d144
gil-kornberg <61718766+gil-kornberg@users.noreply.github.com>
2021-04-30 11:25:27 +0300

    Update TransformerGen.py

--- TGAN_BERT/TransformerGen.py
+++ TGAN_BERT/TransformerGen.py
@@ -11,21 +11,15 @@ noiseDim = 1024  # dimension of noise vector
 device = 'cuda'
 
 encoder_layer = nn.TransformerEncoderLayer(d_model=768, nhead=8).to(device)
-# positional_encoding_layer = positionalEncoding.PositionalEncoding(d_model=768, max_len=seqLen)
 
 class Generator(nn.Module):
 
     def __init__(self):
         super(Generator, self).__init__()
-        # instantiate positional encoding 
         self.gen = nn.TransformerEncoder(encoder_layer, num_layers=6).to(device)
         
     # given a noise tensor return a generated "BERT" embedding
     def forward(self, noise):
-        # noise = self.posencoder(noise)
-        # print("%%%%%%%")
-        # print(type(noise))
-        # print(next(self.gen.parameters()).is_cuda)
         return self.gen(noise).to(device)
 
 
@@ -33,4 +27,3 @@ def get_noise(z_dim):
     t = torch.empty(z_dim)
     nn.init.uniform_(t, a=0.0, b=1.0)
     return t.to(device)
-    #return torch.randn(z_dim, device=device)
